6f672783916a3ec5c5ea62568503c9de8c908c88
John McCabe-Dansted <gmatht@gmail.com>
2014-04-13 13:20:32 +0800

    Added slightly ungolfed version

---
+++ misc/simon2.sh
@@ -0,0 +1,8 @@
+cat <<"EOF"|sed s/E/`echo -e '\E'`/>simon_golf.sh;bash simon_golf.sh;wc simon_golf.sh
+d(){ echo 'Ecx1r09mRx2g10mG
+x3y11mYx4b14mBx0m'$s|sed s/.$1"//
+s/[rgyb]..//g
+s/x/E[48;5;/g";};x(){ d $c;espeak $c;d j;};l(){
+for c in $o;{ eval $1;x;};};f(){ o=$o\ `tr -dc yrgb</d*/ur*|head -c1`
+l;l 'read -n1 i;[ $c = $i ]||exit;let s++';sleep 1;f;};f
+EOF
