60c1b7ec2ccca8c61fa2f0efeccba1cf4667bb15
John McCabe-Dansted <gmatht@gmail.com>
2014-12-23 05:11:47 -0800

    fix bug in nonmono.sh

--- c/nonmono.sh
+++ c/nonmono.sh
@@ -1,8 +1,11 @@
 if [ -z $2 ]
 then
-	echo Usage $0 n_colors n_edges
+	echo Usage $0 n_colors n_vertices
 else
-	sed "s/17/$2/g
-s/4/$1/g" < nonmono.c > nonmono_.c &&
-	gcc nonmono_.c && ./a.out
+	sed "
+s/17/n_vertices/g
+s/4/$1/g
+s/n_vertices/$2/g
+" < nonmono.c > nonmono_.c &&
+	gcc -O3 nonmono_.c && ./a.out
 fi
