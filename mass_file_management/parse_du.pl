7fd188ff83f5e4b11a64334fad052d9b15123601
John McCabe-Dansted <gmatht@gmail.com>
2015-04-01 16:46:53 +0800

    Add more information to disk usage reports, including by disk usage by extension

--- mass_file_management/parse_du.pl
+++ mass_file_management/parse_du.pl
@@ -1,5 +1,8 @@
 my %filereps = ();
+my %sparse_used =();
 my $lastpath="";
+use integer;
+$|=1;
 while (my $line = <>) {
 	if ($line =~ /([^\t]*)\t([^\t]*)\t([^\n\r]*)/) {
 		my $size=$1; my $date=$2; my $path=$3;
@@ -17,14 +20,34 @@ while (my $line = <>) {
 		if (not $isdir) {
 			my $fname=$path;
 			$fname=~ s/.*\///;
+
+			if ($size =~ /([0-9]*)b([0-9]*)/) {
+				$size=$1;
+				my $blocks=$2;
+				my $used=$blocks*512;
+				my $id="$size\t$fname";
+				#print "$id -> Sparse Size: $used ($blocks 512 byte blocks) $path\n";
+				#print "$size -> Sparse Size: $used ($blocks 512 byte blocks) $path\n";
+				if (defined $sparse_used{$id}) {
+					if ($used < $sparse_used{$id}) {
+						$sparse_used{$id}=$used;
+					}
+				} else {
+					$sparse_used{$id}=$used;
+				}	
+			}
+
+
 			my $id="$size\t$fname";
 			if (not $fname =~ /.*[.].*/){
-				print "$id '$path'\n";
+				#print "$id '$path'\n";
+				#print "$id '$path'\r";
 			}
-			print "$id\n";
+			#print "$id\n";
 			$filereps{$id}++;
 
 			#print "/";
+
 		}
 		#print "\n";
 		#print("$isdir:$3, $lastpath <- $3\n");
@@ -35,18 +58,55 @@ while (my $line = <>) {
 	
 }
 	
+sub commify {
+	my $input = shift;
+	$input = reverse $input;
+	$input =~ s/(\d\d\d)(?=\d)(?!\d*\.)/$1,/g;
+	return reverse $input;
+}
+
+my %extension = ();
+for (keys %filereps) {
+	if (/([0-9]*)\t.*[.]([[:alnum:]]*)/) {
+		$extension{uc($2)}+=$1;
+	}
+}
+for (keys %extension) {
+	if ($extension{$_} > 1000000000) {
+		print (($extension{$_}/1000000000)."G\t$_\n");
+	}
+}	
+undef %extension;
+
+foreach my $m (0,1,10) {
+foreach my $u (0..1) {
 
 my $bytes_raw=0; my $bytes_dedup=0;
 my $files_raw=0; my $file_dedup=0;
 for (keys %filereps) {
-	#print "$_ $filereps{$_}\n"
-	$files_raw   += $filereps{$_};
-	$files_dedup += 1;
-	$bytes_dedup  += $_;  
-	$bytes_raw += $filereps{$_}*$_;  
-	#print "$filereps{$_}\n";
+	my $bytes = $_;
+	if ($bytes < 1000000000*$m or (not $m)) {
+		if (defined $sparse_used{$_} and $u) {
+			$bytes = $sparse_used{$_};
+			#print "$_ -> $bytes\n";
+		}
+		#print "$_ $filereps{$_}\n"
+		$files_raw   += $filereps{$_};
+		$files_dedup += 1;
+		$bytes_dedup  += $bytes;  
+		$bytes_raw += $filereps{$_}*$bytes;  
+		#print "$filereps{$_}\n";
+	}
 }
 
-printf "$files_raw\t$bytes_raw\n";
-printf "$files_dedup\t$bytes_dedup\n";
+$files_raw=commify($files_raw); $bytes_raw=commify($bytes_raw);
+$files_dedup=commify($files_dedup); $bytes_dedup=commify($bytes_dedup);
 
+
+my $fmt="%8s%15s%25s\n";
+printf "\n-- USED_SIZE: $u\t LIMIT_TO_GB: $m -- \n";
+printf ($fmt, "",      "FILES",      "BYTES");
+printf ($fmt, "RAW",   $files_raw,   $bytes_raw);
+printf ($fmt, "DEDUP", $files_dedup, $bytes_dedup);
+
+}}
