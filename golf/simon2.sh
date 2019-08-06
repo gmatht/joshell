9d253ce063334c40447ed91f7d9e26c1ef3ba892
gmatht <gmatht@gmail.com>
2019-08-06 14:58:03 +0800

    Add support for windows

--- golf/simon2.sh
+++ golf/simon2.sh
@@ -6,3 +6,10 @@ s/x/E[48;5;/g";};x(){ d $c;espeak $c;d j;};l(){
 for c in $o;{ eval $1;x;};};f(){ o=$o\ `tr -dc yrgb</dev/urandom|head -c1`
 l;l 'read -n1 i;[ $c = $i ]||exit;let s++';sleep 1;f;};f
 EOF
+
+cat <<"EOF" > win10_simon_golf.sh
+espeak(){
+        /mnt/c/Windows/SysWOW64/WindowsPowerShell/v1.0/PowerShell.exe -Command "Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('$1');"
+}
+EOF
+cat simon_golf.sh >> win10_simon_golf.sh
